54233c36f5770affb8c05c707e99cf4697761e9a
Conor Molloy <from.conor.molloy@gmail.com>
2018-05-30 13:55:45 +0100

    Initial Commit

---
+++ Window Shopper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Window Shopper
+//
+//  Created by Conor Molloy on 30/05/2018.
+//  Copyright © 2018 Conor Molloy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
